0a07500ed1382a1f0bde467d122bfafddd21a12a
milanrichardson <mil.an55@yahoo.com>
2016-03-08 10:08:05 -0500

    first commit

---
+++ BeestroApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BeestroApp
+//
+//  Created by python on 3/8/16.
+//  Copyright © 2016 thettyl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
